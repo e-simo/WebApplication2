1963e9babb552029fc1d77f87961114fc26e88dd
Emis SIMO <E.Simo@iaea.org>
2020-06-08 13:56:53 +0200

    Add project files.

---
+++ WebApplication2/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication2.Controllers
+{
+    public class TestController : ApiController
+    {
+        [HttpGet]
+        [Route("api/sample")]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Response ", "Web2" };
+        }
+    }
+}
